f8547378b7de276d75613d1922eb1ea0804be337
Arius Bronte <ariusbronte@gmail.com>
2020-05-16 21:18:05 +0000

    added app middlewares

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use('/api/v1/users', require('./routes/v1/controllers/users'));
 
