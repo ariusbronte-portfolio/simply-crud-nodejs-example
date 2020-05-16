94eab096938324ed782d7542834ba8ef578749cf
Arius Bronte <ariusbronte@gmail.com>
2020-05-16 22:57:31 +0000

    created user add action

--- routes/v1/services/users/userService.js
+++ routes/v1/services/users/userService.js
@@ -1,9 +1,17 @@
 const database = require('../../../../database/connection');
 
+require('../../../../extensions/dateExtensions');
+
 const userService = {
     get: async function get() {
         const query = 'SELECT * FROM Users ORDER BY Id';
 
+        return await database.makeQuery(query);
+    },
+    post: async function post(values) {
+        const query = `INSERT INTO Users (FirstName, LastName, CreationHistory) VALUES (?, ?, '${new Date().toUTC()}')`;
+
+        return await database.makeQuery(query, values);
     }
 }
 
--- routes/v1/users/add.js
+++ routes/v1/users/add.js
@@ -1,7 +1,31 @@
 'use strict'
 
 const router = require('express').Router();
+const {userService} = require('../services/users/userService');
 
-// Code here
+const Joi = require('@hapi/joi');
+const schema = Joi.object({
+    first_name: Joi.string()
+        .min(2)
+        .max(20)
+        .required(),
+
+    last_name: Joi.string()
+        .min(2)
+        .max(20)
+        .required()
+})
+
+router.post('/', async (req, res, next) => {
+    const {error} = await schema.validateAsync(req.body);
+    if (error) return res.status(422).send(error);
+
+    const first_name = req.body.first_name;
+    const last_name = req.body.last_name;
+
+    await userService.post([first_name, last_name]);
+
+    return res.status(200).send();
+});
 
 module.exports = router;
