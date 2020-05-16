766ca3e6a9680395f4f66896a35645ec743494f3
Arius Bronte <ariusbronte@gmail.com>
2020-05-16 21:21:36 +0000

    rename getService to userService

--- routes/v1/users/get.js
+++ routes/v1/users/get.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const router = require('express').Router();
-const { userService } = require('../services/users/getService');
+const { userService } = require('../services/users/userService');
 
 router.get('/', async (req, res) => {
     return res.status(200).send(await userService.get());
