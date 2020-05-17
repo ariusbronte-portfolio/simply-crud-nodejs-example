157a42d7244e53f9b206ed077d9aaa9faa6c5072
Arius Bronte <ariusbronte@gmail.com>
2020-05-17 01:01:15 +0000

    cleanup code

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const {ErrorHandler, handleError} = require("./helpers/error");
+const {handleError} = require("./helpers/error");
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use('/api/v1/users', require('./routes/v1/controllers/users'));
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
     handleError(err, res);
 });
 
